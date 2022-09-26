63113152d14f9265c5becaa29901186e3e2734f4
NoriNerd <bashfulpandajunk@gmail.com>
2022-09-26 10:39:45 -0400

    Started Arm Controls

--- SoftwareControlsPython.py
+++ SoftwareControlsPython.py
@@ -60,82 +60,105 @@ class main:
             wheelsFinal = [lw1, lw2, lw3, rw1, rw2, rw3]
             wheels = [l1, l2, l3, r1, r2, r3] 
             
-            #Increment the motor turn??
-            for i in range(128): #Increment motor starting from default (half length of 255)
-                for wheel in range(len(wheelsFinal)):
-                    if wheelsFinal[wheel] < 128:
-                        wheels[wheel] -= 1    
-                    elif wheelsFinal[wheel] > 128:
-                        wheels[wheel] += 1
-                    
+         
+            for wheel in range(len(wheelsFinal)):
                     #Check within boundaries
-                    if wheels[wheel] > 255:
-                        wheels[wheel] = 255
-                    elif wheels[wheel] < 0:
-                        wheels[wheel] = 0
+                if wheels[wheel] > 255:
+                    wheels[wheel] = 255
+                elif wheels[wheel] < 0:
+                    wheels[wheel] = 0
                 
-                packet.msg= "DriveCommand_" + str(lw1) + "_" + str(rw1) + "_" + str(lw2) + "_" + str(rw2) + "_" + str(lw3) + "_" + str(rw3)
+            packet.msg= "DriveCommand_" + str(lw1) + "_" + str(rw1) + "_" + str(lw2) + "_" + str(rw2) + "_" + str(lw3) + "_" + str(rw3)
 
-                if packet.pastmsg != packet.msg:
-                    print(packet.msg)
-                host.sendto(packet.msg.encode(), compIp, serverPort)
+            if packet.pastmsg != packet.msg:
+                print(packet.msg)
+            host.sendto(packet.msg.encode(), compIp, serverPort)
 
 
         #Keyboard Inputs
-        #Initializing controller
+        #Initializing controller(s): 1 for arm, 1 for driving
         pygame.init()
         pygame.joystick.init()
         joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+        if pygame.joystick.get_count() != 0:
+            driveJoystick = joysticks[0]
+            if pygame.joystick.get_count() == 2:
+                armJoystick = joysticks[1]
+
 
 
         while True:
             for event in pygame.event.get():
+
+                #Reinitializing Joysticks
+                if event.type == pygame.JOYDEVICEADDED or event.type == pygame.JOYDEVICEREMOVED:
+                    joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+                    if pygame.joystick.get_count() != 0:
+                        driveJoystick = joysticks[0]
+                        if pygame.joystick.get_count() == 2:
+                            armJoystick = joysticks[1]
+
+             
+
                 #Drive controls
-                if event.type == pygame.JOYAXISMOTION:
-                    if event.axis < 2:
-                        #Get controllers' input
-                        leftMotion = joysticks[0].get_axis(leftStick)
-                        rightMotion = joysticks[0].get_axis(rightStick)
-                        rTriggerMotion = joysticks[0].get_axis(rightTrigger)
-                        lTriggerMotion = joysticks[0].get_axis(leftTrigger)
+                if event.joy == driveJoystick:
+                    if event.type == pygame.JOYAXISMOTION:
+                        if event.axis < 2:
+                            #Get controllers' input
+                            leftMotion = driveJoystick.get_axis(leftStick)
+                            rightMotion = driveJoystick.get_axis(rightStick)
+                            rTriggerMotion = driveJoystick.get_axis(rightTrigger)
+                            lTriggerMotion = driveJoystick.get_axis(leftTrigger)
 
-                        #Dead Zone
-                        if abs(event.value) < threshhold:
-                            drive()            
-                            continue       
+                            #Dead Zone
+                            if abs(event.value) < threshhold:
+                                drive()            
+                                continue       
 
-                        #Check which stick on the controller moves
-                        #Simultaneous Motion
-                        elif abs(leftMotion) > threshhold and abs(rightMotion) > threshhold:
-                            leftSideSpeed = leftMotion * 170
-                            rightSideSpeed = rightMotion * 170
-                            #drive(170 * leftMotion, 170 * leftMotion, 170 * leftMotion, 170 * rightMotion, 170 * rightMotion, 170 * rightMotion) # Set event value of each axis to multiply direction
-                        #Turn left
-                        elif abs(leftMotion) > threshhold:
-                            leftSideSpeed = leftMotion * 170
-                            rightSideSpeed = rightMotion * 70
-                            #drive(lw1 = 170 * leftMotion, lw2 = 170 * leftMotion, lw3 = 170 * leftMotion, rw1 = 70, rw2 = 70, rw3 = 70) 
-                        #Turn right
-                        elif abs(rightMotion) > threshhold:
-                            leftSideSpeed = leftMotion * 70
-                            rightSideSpeed = rightMotion * 170
-                            drive(rw1 = 170 * rightMotion, rw2 = 170 * rightMotion, rw3 = 170 * rightMotion, lw1 = 70, lw2 = 70, lw3 = 70) 
-                        
-                        
-                        #Speed Increase
-                        elif abs(rTriggerMotion) > threshhold:
-                            leftSideSpeed += 70
-                            rightSideSpeed += 70
-                        
-                        #Speed Decrease
-                        elif abs(lTriggerMotion) > threshhold:
-                            leftSideSpeed -= 70
-                            rightSideSpeed -=70
-                        
-                        drive(leftSideSpeed, leftSideSpeed, leftSideSpeed, rightSideSpeed, rightSideSpeed, rightSideSpeed)
+                            #Check which stick on the controller moves
+                            #Simultaneous Motion
+                            elif abs(leftMotion) > threshhold and abs(rightMotion) > threshhold:
+                                leftSideSpeed = leftMotion * 170
+                                rightSideSpeed = rightMotion * 170
+                                #drive(170 * leftMotion, 170 * leftMotion, 170 * leftMotion, 170 * rightMotion, 170 * rightMotion, 170 * rightMotion) # Set event value of each axis to multiply direction
+                            #Turn left
+                            elif abs(leftMotion) > threshhold:
+                                leftSideSpeed = leftMotion * 170
+                                rightSideSpeed = rightMotion * 70
+                                #drive(lw1 = 170 * leftMotion, lw2 = 170 * leftMotion, lw3 = 170 * leftMotion, rw1 = 70, rw2 = 70, rw3 = 70) 
+                            #Turn right
+                            elif abs(rightMotion) > threshhold:
+                                leftSideSpeed = leftMotion * 70
+                                rightSideSpeed = rightMotion * 170
+                                drive(rw1 = 170 * rightMotion, rw2 = 170 * rightMotion, rw3 = 170 * rightMotion, lw1 = 70, lw2 = 70, lw3 = 70) 
                             
-            
+                            
+                            #Speed Increase
+                            elif abs(rTriggerMotion) > threshhold:
+                                leftSideSpeed += 70
+                                rightSideSpeed += 70
+                            
+                            #Speed Decrease
+                            elif abs(lTriggerMotion) > threshhold:
+                                leftSideSpeed -= 70
+                                rightSideSpeed -=70
+                            
+                            drive(leftSideSpeed, leftSideSpeed, leftSideSpeed, rightSideSpeed, rightSideSpeed, rightSideSpeed)
+                                
+                
                 #Arm Control
+                #Hoist, joystick
+                #Upper and lower, idk the triggers?
+                #Claw, button
+                #Swivel, other joystick??????
+                if event.joy == armJoystick:
+                    if event.type == pygame.JOYAXISMOTION:
+                        if event.axis < 2:
+                            hoistMotion = armJoystick.get_axis(leftStick)
+                            swivelMotion = armJoystick.get_axis(rightStick)
+                            
+
+            
                     
                 
 if __name__ == "__main__":
