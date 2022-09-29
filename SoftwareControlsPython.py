347920b38ec5fa7304b9c254a04c30ae4e6f5863
NoriNerd <bashfulpandajunk@gmail.com>
2022-09-29 13:19:04 -0400

    Arm controls added

--- SoftwareControlsPython.py
+++ SoftwareControlsPython.py
@@ -22,6 +22,11 @@ class main:
         rightTrigger = 4
         leftTrigger = 5
         threshhold = 0.1
+        crossButton = 1
+        triButton = 2
+        sqrButton = 3
+        circButton = 4
+
         #Pin unneeded in Python 
         #Drive Pin Numbers
         leftW1 = 11
@@ -42,12 +47,22 @@ class main:
         claw = 28
         swivel = 12
 
-        packet = data()
+        
+        screwMove = 128
+        hoistMove = 150
+        swivelMove = 128
+        clawMove = 128
+        upExtMove = 128
+        lowExtMove = 128
         leftSideSpeed = 128
         rightSideSpeed = 128
-        
 
-        packet.pastmsg = ""
+        drivePacket = data()
+        armPacket = data()
+        
+        armPacket.pastmsg = ""
+        drivePacket.pastmsg = ""
+
         #HOPEFULLY allows Python to communicate with Arduino via sockets
         host = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #host.bind(compIp, serverPort) #Talk to Arduino
@@ -65,14 +80,31 @@ class main:
                 elif wheels[wheel] < 0:
                     wheels[wheel] = 0
                 
-            packet.msg= "DriveCommand_" + str(lw1) + "_" + str(rw1) + "_" + str(lw2) + "_" + str(rw2) + "_" + str(lw3) + "_" + str(rw3)
+            drivePacket.msg= "DriveCommand_" + str(lw1) + "_" + str(rw1) + "_" + str(lw2) + "_" + str(rw2) + "_" + str(lw3) + "_" + str(rw3)
 
-            if packet.pastmsg != packet.msg:
-                print(packet.msg)
-            host.sendto(packet.msg.encode(), compIp, serverPort)
+            if drivePacket.pastmsg != drivePacket.msg:
+                print(drivePacket.msg)
+                host.sendto(drivePacket.msg.encode(), compIp, serverPort)
         
         def arm(upExt = 128, lowExt = 128, hoist = 150, screw = 128, claw = 128, swivel = 128):
-            up, low, h, sc, cl, sw = 128
+            
+            #Unlike other comps, hoist can not go to 128
+            if hoist < 150:
+                hoist = 150
+
+            armComps = [upExt, lowExt, hoist, screw, claw, swivel]
+            for comp in range(len(armComps)):
+                if armComps[comp] > 255:
+                    armComps[comp] = 255
+                elif armComps[comp] < 0:
+                    armComps[comp] = 0
+    
+            armPacket.msg = "ArmCommand_" + upExt + "_" + lowExt + "_" + screw + "_" + claw + "_" + hoist + "_" + swivel
+
+            if armPacket.pastmsg != armPacket.msg: 
+                print(armPacket.msg)
+                host.sendto(drivePacket.msg.encode(), compIp, serverPort)
+                
 
 
         #Keyboard Inputs
@@ -130,11 +162,11 @@ class main:
                             elif abs(rightMotion) > threshhold:
                                 leftSideSpeed = leftMotion * 70
                                 rightSideSpeed = rightMotion * 170
-                                drive(rw1 = 170 * rightMotion, rw2 = 170 * rightMotion, rw3 = 170 * rightMotion, lw1 = 70, lw2 = 70, lw3 = 70) 
+                                #drive(rw1 = 170 * rightMotion, rw2 = 170 * rightMotion, rw3 = 170 * rightMotion, lw1 = 70, lw2 = 70, lw3 = 70) 
                             
                             
                             #Speed Increase
-                            elif abs(rTriggerMotion) > threshhold:
+                            if abs(rTriggerMotion) > threshhold:
                                 leftSideSpeed += 70
                                 rightSideSpeed += 70
                             
@@ -148,15 +180,71 @@ class main:
                 
                 #Arm Control
                 #Hoist, joystick
-                #Upper and lower, idk the triggers?
-                #Claw, button
+                #Upper and lower, buttons?
+                #Claw, trigger
                 #Swivel, other joystick??????
                 if event.joy == armJoystick:
                     if event.type == pygame.JOYAXISMOTION:
                         if event.axis < 2:
+                            #Joystick Motion Controls
                             hoistMotion = armJoystick.get_axis(leftStick)
                             swivelMotion = armJoystick.get_axis(rightStick)
+                            clawCloseMotion = armJoystick.get_axis(leftTrigger)
+                            clawOpenMotion = armJoystick.get_axis(rightTrigger)
+                            upExtMove = 128
+                            lowExtMove = 128
+                            screwMove = 128
+
+
+                            if abs(event.value) < threshhold:
+                                arm()            
+                                continue       
+
+                            #Check which stick on the controller moves
+                            #Simultaneous Motion
+                            elif abs(hoistMotion) > threshhold and abs(swivelMotion) > threshhold:
+                                hoistMove = hoistMotion * 170
+                                swivelMove = swivelMotion * 170
+
+                            elif abs(hoistMotion) > threshhold:
+                                hoistMove = hoistMotion * 170
+                                swivelMove = swivelMotion * 70
+                                #drive(lw1 = 170 * leftMotion, lw2 = 170 * leftMotion, lw3 = 170 * leftMotion, rw1 = 70, rw2 = 70, rw3 = 70) 
+                            #Turn right
+                            elif abs(swivelMotion) > threshhold:
+                                hoistMove = leftMotion * 70
+                                swivelMove = rightMotion * 170
+                                drive(rw1 = 170 * rightMotion, rw2 = 170 * rightMotion, rw3 = 170 * rightMotion, lw1 = 70, lw2 = 70, lw3 = 70) 
+                            
+                            
+                            #Claw Open
+                            if abs(clawOpenMotion) > threshhold:
+                                clawMove = clawOpenMotion * 170
+                            
+                            #Claw Close
+                            elif abs(clawCloseMotion) > threshhold:
+                                clawMove = clawOpenMotion * 70
+                            
+                            arm(hoist = hoistMove, claw = clawMove, swivel = swivelMove )
+                    
+                    if event.type == pygame.JOYBUTTONDOWN:
+                        if armJoystick.get_button(crossButton):
+                            upExtMove += 70
+                        elif armJoystick.get_button(triButton):
+                            upExtMove -= 70
+                        if armJoystick.get_button(sqrButton):
+                            lowExtMove += 70
+                        elif armJoystick.get_button(circButton):
+                            lowExtMove -= 70
+                    
+                    if event.type == pygame.JOYHATMOTION:
+                        screwX, screwY = event.value
+                        if screwX == -1:
+                            screwMove -= 20
+                        elif screwX == 1:
+                            screwMove += 20
                         
+
                         
 
 
